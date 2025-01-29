623ba752a6c43dd03d1e335cd8a82f39a2822131
lyuhyewon <lyuhw11023@gmail.com>
2025-01-29 20:24:06 +0900

    AppBar 및 TabBar 초기 구현

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
     required this.child,
     this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
     Key? key,
 }) : super(key: key);
 
@@ -14,7 +18,29 @@ class DefaultLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar(){
+    if(title == null) {
+      return null;
+    }else{
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0, // 앞으로 튀어나오는 효과
+        title: Text(
+          title!, // !는 절대로 null값이 될 수 없다는 의미 - 이미 if 문으로 null 처리 해줌
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
